e81f93d4d8952ae6a4bdeb9b11161fa6cfdd0db3
Alberto F. Martín <amartin@cimne.upc.edu>
2017-03-22 20:38:00 +0100

    First version of template SLURM script required to execute test_hw_openmp on a single TITANI's compute node

---
+++ template_slurm_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --partition=parallel
+#SBATCH --output=test_hw_openmp_%j.stdout
+#SBATCH --error=test_hw_openmp_%j.stderr
+#SBATCH --cpus-per-task=24
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+#Please set up this environment variable such that it points to
+#the proper ABSOLUTE path where the compiled test_hw_openmp
+DRIVER_PROGRAM_ABS_PATH=$HOME/mnme_ddlssc_hw_openmp_skeleton/test_hw_openmp
+
+#The value of this environment variable should be modified in order
+#to play around different SCHEDULE STRATEGIES and several chunk sizes
+#(as required by the OpenMP HW)
+export OMP_SCHEDULE=STATIC
+
+export OMP_DISPLAY_ENV=VERBOSE
+for NUM_THREADS in 1 2 4 8 16 24
+do
+   export OMP_NUM_THREADS=$NUM_THREADS
+   echo "*****************"
+   echo "BEGIN OMP_NUM_THREADS=$OMP_NUM_THREADS RESULTS"
+   OMP_PLACES_VALUE="{0}"
+   i=1
+   while [ "$i" -lt "$NUM_THREADS" ]
+   do
+     OMP_PLACES_VALUE="$OMP_PLACES_VALUE,{$i}"
+     let i=i+1
+   done
+   export OMP_PLACES=$OMP_PLACES_VALUE 
+
+   #Execute driver program
+   $DRIVER_PROGRAM_ABS_PATH   
+
+   echo "END OMP_NUM_THREADS=$OMP_NUM_THREADS RESULTS"
+   echo "*****************"
+done
+
